24cd6b627f38b31c604962383504a4adccee3d16
Griffin Edwards <77746705+Griffin-Edwards919@users.noreply.github.com>
2025-04-01 17:37:15 -0400

    Update raw_computation.c

--- raw_computation.c
+++ raw_computation.c
@@ -37,11 +37,6 @@ void some_computation(long iterations) {
     printf("Final sum: %d\n", sum);
 }
 
-static long perf_event_open(struct perf_event_attr *hw_event, pid_t pid, 
-                            int cpu, int group_fd, unsigned long flags) {
-    return syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd, flags);
-}
-
 double measure_events(long iterations, int enable_counters) {
 
     setup_array();
